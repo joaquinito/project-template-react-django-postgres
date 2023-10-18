a91a2865f74d4b5cc0daaeef5e175d10d1b04eb0
Ricardo Joaquinito <rjoaquinito@gmail.com>
2023-10-18 20:06:21 +0200

    Fixed a serialization issue

--- server/rest_api/serializers.py
+++ server/rest_api/serializers.py
@@ -4,10 +4,14 @@ from .models import Vehicle
 # Serializers are a way to convert objects to and from Python objects. It takes a JSON input that
 # might be posted from the API, validates the input to make sure that it is secure and correct as
 # per our validation rules and then it converts it either to a Python object or a model in our 
-# actual database.
+# actual database. It also does the reverse, which is to convert a Python object or a model to a
+# JSON output. 
 
 class VehicleSerializer(serializers.ModelSerializer):
+
+    numberWheels = serializers.IntegerField(source='number_wheels')
+
     class Meta:
         model = Vehicle
-        fields = '__all__'
+        fields = ['id', 'name', 'type', 'numberWheels']
         read_only_fields = ['id']
